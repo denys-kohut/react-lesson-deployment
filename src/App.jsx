2f7938fb6f9eeac70f210014f57eed87dd2dc56e
Denys Kohut <kohut.den@gmail.com>
2024-06-18 23:50:22 +0200

    renamed file for Vercel

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import { List } from './components/List';
 import { Disclaimer } from './components/Disclaimer';
 import { Theme } from './constants';
 import { Counter } from './components/Counter';
-import './App.css';
+import './AppStyle.css';
 
 export function App({disclaimerText, title, theme, goods}) {
   const [counter, setCounter] = useState(0)
